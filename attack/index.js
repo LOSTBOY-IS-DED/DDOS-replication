fb4e61f8fca2f3cc5abadcbb2aa5fc5e3f722414
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-10-06 00:18:13 +0530

    step 2

--- attack/index.js
+++ attack/index.js
@@ -1,25 +1,32 @@
 const axios = require("axios");
-let data = JSON.stringify({
-  email: "subh@gmail.com",
-  otp: 230653,
-  newPassword: "hola-amigo",
-});
 
-let config = {
-  method: "post",
-  maxBodyLength: Infinity,
-  url: "http://localhost:3000/reset-password",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  data: data,
-};
-
-axios
-  .request(config)
-  .then((response) => {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch((error) => {
-    console.log(error);
+async function sendRequest(otp) {
+  const data = JSON.stringify({
+    email: "subh@gmail.com",
+    otp: otp,
+    newPassword: "hola-amigo",
   });
+
+  const config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "http://localhost:3000/reset-password",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  try {
+    const response = await axios.request(config);
+    console.log(JSON.stringify(response.data));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function main() {
+  await sendRequest("210116"); // Call the sendRequest function once
+}
+
+main();
